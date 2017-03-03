f105e70fcc059378dffb4f6e66c11057d90c98d6
Jun Ito <jun_ito@voyagegroup.com>
2017-03-03 20:07:41 +0900

    Update: change mode with query param

--- src/app.js
+++ src/app.js
@@ -16,29 +16,37 @@ document.addEventListener('DOMContentLoaded', function () {
     } else {
         scene.addEventListener('loaded', main)
     }
+})
 
-    function main() {
-        setModeIfNeeded()
-        const ball = document.getElementById('ball')
-        setTimeout(() => {
-            ball.emit('startGame')
-        }, 1000)
-    }
+function main() {
+    setModeIfNeeded()
+    const ball = document.getElementById('ball')
+    setTimeout(() => {
+        ball.emit('startGame')
+    }, 1000)
+}
 
-    function setModeIfNeeded() {
-        const queryDict = {}
+function setModeIfNeeded() {
+    const queryDict = {}
+
+    window.location.search.substr(1).split('&').forEach((item) => {
+        queryDict[item.split('=')[0]] = item.split('=')[1]
+    })
+
+    if ('mode' in queryDict) {
         const modeList = { 'easy': 0.05, 'normal': 0.1, 'hard': 0.2, 'superhard': 0.3 }
-
-        window.location.search.substr(1).split('&').forEach((item) => {
-            queryDict[item.split('=')[0]] = item.split('=')[1]
-        })
-
         const efficiency = modeList[queryDict.mode]
-
         if (efficiency) {
             const enemy = document.getElementById('enemy')
             enemy.setAttribute('enemy', 'efficiency', efficiency)
         }
     }
-})
+
+    if ('head' in queryDict) {
+        const headControls = document.getElementById('head-controls')
+        const cam = document.querySelector('a-camera')
+        headControls.setAttribute('visible', true)
+        cam.setAttribute('position', '0 1.2 1')
+    }
+}
 
